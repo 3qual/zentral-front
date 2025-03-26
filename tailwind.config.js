0ab6335c77b1becc54409bb8012a17adb2311988
Max <maksimmun007@gmail.com>
2025-03-27 01:25:26 +0900

    feat(tailwind): enhance color palette with light and dark variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,31 @@ module.exports = {
       colors: {
         white: '#faf9f6',
         black: '#000',
-        red: '#ff595e',
-        yellow: '#ffca3a',
-        green: '#8ac926',
-        blue: '#1982c4',
-        purple: '#6a4c93',
+        red: {
+          'light': '#ffc7c9',
+          'DEFAULT': '#ff595e',
+          'dark': '#e51d23'
+        },
+        yellow: {
+          'light': '#fff2c6',
+          'DEFAULT': '#ffca3a',
+          'dark': '#f99707'
+        },
+        green: {
+          'light': '#bce76f',
+          'DEFAULT': '#8ac926',
+          'dark': '#4d7417',
+        },
+        blue: {
+          'light': '#4caee4',
+          'DEFAULT': '#1982c4',
+          'dark': '#145078',
+        },
+        purple: {
+          'light': '#9f82ca',
+          'DEFAULT': '#6a4c93',
+          'dark': '#4f3a69'
+        }
       }
     },
   },
