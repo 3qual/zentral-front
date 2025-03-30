139e72e5c6aacd0378ac38f91f8066848610ff5a
Max <maksimmun007@gmail.com>
2025-03-30 14:51:36 +0900

    feat(api): add createTransaction function

---
+++ api/transactions/transactions.api.ts
@@ -0,0 +1,22 @@
+import { API } from "@/constants";
+import { ApiResult } from "@/types/api/Responses";
+import { Transaction, TransactionCreateInput } from "@/types/transactions/Transaction";
+
+export const transactionsApi = {
+  async createTransaction(transaction: TransactionCreateInput): Promise<ApiResult<Transaction>> {
+    const response = await fetch(`${API.transactions}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(transaction),
+    });
+
+    const data = await response.json();
+    
+    return {
+      response,
+      data: data as Transaction
+    };
+  }
+}
